a8689226d74c1fd19fc37bf9998d571c913f49a7
Spyd3r05 <agnesnzivu05@gmail.com>
2026-03-24 08:56:14 +0300

    feat: define shared types

---
+++ app/types.ts
@@ -0,0 +1,12 @@
+//define shared types
+export interface Opportunity {
+  id: number;
+  title: string;
+  company: string;
+  domain: string;
+  location: string;
+  time: string;
+  tags: string[];
+  isNew: boolean;
+  link: string;
+}
